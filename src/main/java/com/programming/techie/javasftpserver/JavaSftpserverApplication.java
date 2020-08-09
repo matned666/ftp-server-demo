3cc8d69e72237913fcead1e2aed6950adc465bde
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2020-08-09 14:10:52 +0200

    basic config

--- src/main/java/com/programming/techie/javasftpserver/JavaSftpserverApplication.java
+++ src/main/java/com/programming/techie/javasftpserver/JavaSftpserverApplication.java
@@ -1,14 +1,45 @@
 package com.programming.techie.javasftpserver;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.event.inbound.ApplicationEventListeningMessageProducer;
+import org.springframework.integration.ftp.server.ApacheMinaFtpEvent;
+import org.springframework.integration.ftp.server.ApacheMinaFtplet;
+import org.springframework.messaging.MessageChannel;
 
 @SpringBootApplication
 public class JavaSftpserverApplication {
 
+	private Log log = LogFactory.getLog(JavaSftpserverApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSftpserverApplication.class, args);
 		while (true);
+	}
+
+	@Bean
+	public ApacheMinaFtplet getApacheMinaFtplet(){
+		return new ApacheMinaFtplet();
+	}
+
+	@Bean
+	public ApplicationEventListeningMessageProducer eventsAdapter() {
+		ApplicationEventListeningMessageProducer producer =
+				new ApplicationEventListeningMessageProducer();
+		producer.setEventTypes(ApacheMinaFtpEvent.class);
+		producer.setOutputChannel(eventChannel());
+		return producer;
+	}
+
+	@Bean
+	public MessageChannel eventChannel() {
+		DirectChannel directChannel =  new DirectChannel();
+		directChannel.subscribe(message -> log.info("" + message.getPayload().getClass().getSimpleName()));
+		return directChannel;
 	}
 
 }
--- src/main/java/com/programming/techie/javasftpserver/MySftpServer.java
+++ src/main/java/com/programming/techie/javasftpserver/MySftpServer.java
@@ -2,36 +2,77 @@ package com.programming.techie.javasftpserver;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.sshd.server.SshServer;
-import org.apache.sshd.server.config.keys.AuthorizedKeysAuthenticator;
-import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
-import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.apache.ftpserver.FtpServer;
+import org.apache.ftpserver.FtpServerFactory;
+import org.apache.ftpserver.filesystem.nativefs.NativeFileSystemFactory;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.UserManager;
+import org.apache.ftpserver.listener.ListenerFactory;
+import org.apache.ftpserver.usermanager.PropertiesUserManagerFactory;
+import org.apache.ftpserver.usermanager.SaltedPasswordEncryptor;
+import org.apache.ftpserver.usermanager.impl.BaseUser;
+import org.apache.ftpserver.usermanager.impl.WritePermission;
+import org.springframework.integration.ftp.server.ApacheMinaFtplet;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.IOException;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class MySftpServer {
 
     private Log log = LogFactory.getLog(MySftpServer.class);
 
+    private final ApacheMinaFtplet apacheMinaFtplet;
+
+    public MySftpServer(ApacheMinaFtplet apacheMinaFtplet) {
+        this.apacheMinaFtplet = apacheMinaFtplet;
+    }
+
     @PostConstruct
-    public void startServer() throws IOException {
+    public void startServer() throws FtpException {
         start();
     }
 
-    private void start() throws IOException {
-        SshServer sshd = SshServer.setUpDefaultServer();
-        sshd.setHost("localhost");
-        sshd.setPort(2222);
-        sshd.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(new File("host.ser")));
-        sshd.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
-        sshd.setPasswordAuthenticator((username, password, session) -> username.equals("test") && password.equals("password"));
-        sshd.setPublickeyAuthenticator(new AuthorizedKeysAuthenticator(new File("---Location of authorized_keys ---->")));
-        sshd.start();
-        log.info("SFTP server started");
-    }
+    private void start() throws FtpException {
+
+        FtpServerFactory serverFactory = new FtpServerFactory();
+        ListenerFactory factory = new ListenerFactory();
+
+        // set the port of the listener
+        factory.setPort(2221);
+
+            // replace the default listener
+        serverFactory.addListener("default", factory.createListener());
+        serverFactory.setFtplets(new HashMap<>(Collections.singletonMap("springFtplet", apacheMinaFtplet)));
+
+        PropertiesUserManagerFactory userManagerFactory = new PropertiesUserManagerFactory();
+//        userManagerFactory.setFile(new File("myusers.properties"));
+        userManagerFactory.setPasswordEncryptor(new SaltedPasswordEncryptor());
+        UserManager um = userManagerFactory.createUserManager();
+        BaseUser user = new BaseUser();
+        user.setName("user");
+        user.setPassword("secret");
+        user.setHomeDirectory("ftproot");
+
+        WritePermission writePermission = new WritePermission();
+        user.setAuthorities(List.of(writePermission));
+
+        um.save(user);
+
+        serverFactory.setUserManager(um);
+
+        NativeFileSystemFactory nativeFileSystemFactory = new NativeFileSystemFactory();
+        nativeFileSystemFactory.setCreateHome(true);
+        nativeFileSystemFactory.createFileSystemView(user);
+
+        serverFactory.setFileSystem(nativeFileSystemFactory);
+
+        // start the server
+        FtpServer server = serverFactory.createServer();
+        server.start();
+
+   }
 }
